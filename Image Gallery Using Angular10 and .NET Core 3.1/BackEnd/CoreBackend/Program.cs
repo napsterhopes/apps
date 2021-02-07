5c4b16437e0d903aa9f3995876603dd9be604779
Harshit Gupta <napsterhopes@gmail.com>
2021-02-07 19:09:41 +0530

    Add files via upload

---
+++ Image Gallery Using Angular10 and .NET Core 3.1/BackEnd/CoreBackend/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CoreBackend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Logger.SetLog4NetConfiguration();
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
